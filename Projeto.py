393af0a5abf801591cb4f7480a19362084c2c49f
Raynarak <raynarakerolayne4@gmail.com>
2023-07-17 01:00:11 -0300

    Commit 5

--- Projeto.py
+++ Projeto.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import streamlit as st
 
+df = pd.DataFrame({
+    'date': pd.to_datetime(['2023-01-01', '2023-01-02', '2023-01-03', '2010-01-11', '2011-02-18', '2012-03-12', '2013-03-12', '2014-03-14', '2015-03-20', '2021-06-17']),
+    'adjusted': [100, 101, 102, 50, 47, 39, 48, 75, 12, 35]
+})
+
+df.to_csv('prices.csv')
+
 # Load the CSV file
 df = pd.read_csv('prices.csv')
 
